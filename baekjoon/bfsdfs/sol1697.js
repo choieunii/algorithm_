5b0c01980950b047900b12c38103abfb9f4267dd
choieunii <ghk784@dgu.ac.kr>
2022-03-25 15:31:29 +0900

    add : 백준 1697 bfs

--- baekjoon/bfsdfs/sol1697.js
+++ baekjoon/bfsdfs/sol1697.js
@@ -1,12 +1,37 @@
 let fs = require('fs');
 let input = fs.readFileSync('./dev/stdin.txt').toString().trim().split('\n');
 
-const [N, M] = input
+const [N, K] = input
     .shift()
     .split(' ')
     .map((v) => Number(v));
-const data = input.map((a) => a.split(' ').map((v) => Number(v)));
 
-const solution = (N, M, input) => {};
+const solution = (N, K) => {
+    const visited = new Array(100001).fill(false);
+    const queue = [{ loc: N, time: 0 }];
+    let idx = 0;
 
-solution(N, M, data);
+    while (idx < queue.length) {
+        const cur = queue[idx++];
+
+        if (cur.loc === K) {
+            console.log(cur.time);
+            break;
+        }
+
+        if (cur.loc - 1 >= 0 && !visited[cur.loc - 1]) {
+            visited[cur.loc - 1] = true;
+            queue.push({ loc: cur.loc - 1, time: cur.time + 1 });
+        }
+        if (cur.loc + 1 <= 100000 && !visited[cur.loc + 1]) {
+            visited[cur.loc + 1] = true;
+            queue.push({ loc: cur.loc + 1, time: cur.time + 1 });
+        }
+        if (cur.loc * 2 <= 100000 && !visited[cur.loc * 2]) {
+            visited[cur.loc * 2] = true;
+            queue.push({ loc: cur.loc * 2, time: cur.time + 1 });
+        }
+    }
+};
+
+solution(N, K);
