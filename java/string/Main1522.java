9bd11d90f28cbfd895096ddd53f27503a8115255
choieunii <ghk784@dgu.ac.kr>
2022-04-11 18:22:39 +0900

    add : 백준 1522 문자열 교환

---
+++ java/string/Main1522.java
@@ -0,0 +1,31 @@
+package string;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main1522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int aCnt = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a')
+                aCnt++;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            int bCnt = 0;
+            for (int j = i; j < aCnt + i; j++) {
+                if (str.charAt(j % str.length()) == 'b')
+                    bCnt++;
+            }
+            min = Math.min(min, bCnt);
+        }
+
+        System.out.println(min);
+    }
+}
