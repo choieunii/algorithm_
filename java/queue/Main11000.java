1d750754e58543bd722a57f712e2f616b52ce464
choieunii <ghk784@dgu.ac.kr>
2022-04-13 22:26:07 +0900

    add : 백준 11000 강의실 배정

---
+++ java/queue/Main11000.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main11000 {
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> taskQ = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            pq.offer(new int[] { start, end });
+        }
+
+        while (!pq.isEmpty()) {
+            int[] tmp = pq.poll();
+
+            if (!taskQ.isEmpty() && taskQ.peek() <= tmp[0]) {
+                taskQ.poll();
+            }
+            taskQ.offer(tmp[1]);
+
+        }
+
+        System.out.println(taskQ.size());
+    }
+}
