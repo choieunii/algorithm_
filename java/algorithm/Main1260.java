ff4bbed7536452c117b75ffaa4c850f0a9e9b09d
choieunii <ghk784@dgu.ac.kr>
2022-04-29 02:34:17 +0900

    add : 백준 1260 dfs와 bfs 프로그램

---
+++ java/algorithm/Main1260.java
@@ -0,0 +1,71 @@
+package algorithm;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main1260 {
+    static boolean visited[];
+    static ArrayList<Integer>[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int start = Integer.parseInt(st.nextToken());
+
+        A = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(A[i]);
+        }
+
+        visited = new boolean[N + 1];
+        dfs(start);
+        System.out.println();
+        visited = new boolean[N + 1];
+        bfs(start);
+        System.out.println();
+    }
+
+    public static void dfs(int node) {
+        System.out.print(node + " ");
+        visited[node] = true;
+        for (int i : A[node]) {
+            if (visited[i])
+                continue;
+            dfs(i);
+
+        }
+    }
+
+    public static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            System.out.print(now + " ");
+            for (int i : A[now]) {
+                if (visited[i])
+                    continue;
+                visited[i] = true;
+                queue.add(i);
+            }
+        }
+    }
+}
