cd0e70055e61028f3f1019248a68026fe4991ab2
choieunii <ghk784@dgu.ac.kr>
2022-04-29 01:04:21 +0900

    add : 백준 1253 좋다

---
+++ java/algorithm/Main1253.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main1253 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        long A[] = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        for (int k = 0; k < N; k++) {
+            long find = A[k];
+            int i = 0;
+            int j = N - 1;
+
+            while (i < j) {
+                if (A[i] + A[j] == find) {
+                    if (i != k && j != k) {
+                        result++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else if (j == k) {
+                        j--;
+                    }
+                } else if (A[i] + A[j] < find) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        System.out.println(result);
+        br.close();
+    }
+}
