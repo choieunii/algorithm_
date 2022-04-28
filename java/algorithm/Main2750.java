1ac35c1ce8eb5628801238d75397f6f4512e2504
choieunii <ghk784@dgu.ac.kr>
2022-04-29 01:45:22 +0900

    add : 백준 2750 수정렬하기

---
+++ java/algorithm/Main2750.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (A[j] > A[j + 1]) {
+                    int tmp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
